3d3f910b6efea954cf462f4792ef1fe72d289a14
Sachita Rayapati <rayapats@uci.edu>
2025-04-26 18:17:00 -0700

    Made changes to mobile view menu toggle

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar__menu');
 
-menu.addEventListener('click', () => {
-    menu.classList.toggle('is-active');
-    menuLinks.classList.toggle('active');
+// Toggle mobile menu
+menu.addEventListener('click', function(event) {
+  menu.classList.toggle('is-active');
+  menuLinks.classList.toggle('active');
+  
+  // Prevent the click from bubbling up to document
+  event.stopPropagation();
+});
+
+// Close menu when clicking elsewhere on page
+document.addEventListener('click', function(event) {
+  if (!menu.contains(event.target) && !menuLinks.contains(event.target)) {
+    menu.classList.remove('is-active');
+    menuLinks.classList.remove('active');
+  }
 });
